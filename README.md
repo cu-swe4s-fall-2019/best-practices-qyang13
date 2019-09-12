4863587f1ec45e937d0af615dc41a13f5aac0f3a
Qing Yang <qyang.kevin@gmail.com>
2019-09-11 18:11:05 -0600

    Assignment 1 submission. Fixed style issues in all files, added more complex testing using ssshtest

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# best-practices
+Assignment 1: best-practices
+=============================
+
+# Below is the summary of the changes made:
+
+style.py
+=========
++ Fixed all the style error pointed out by Pycodestyle
+
+get_column_stats.py
+=========
++ Fixed all the style error pointed out by Pycodestyle
++ Wrap most of the operations into functions
++ Added exception handling for numerous instances
++ Added comments for most of the steps
++ Fixed the inconsistency of the indexing. Now 1st column means 1st column, instead of 0th column
+
+basics_test.sh
+=========
++ Utilized ssshtest to test using pycodestyle
++ Added addtional test to make sure the get_column_stats is able to catch all kinds of errors
--- get_column_stats.py
+++ get_column_stats.py
@@ -1,19 +1,82 @@
-import sys, math
+import sys
+import math
+import argparse as ap
 
-file_name = sys.argv[1]
-col_num = int(sys.argv[2])
 
-f = open(file_name, 'r')
+# Function to calculate the mean of a integer vector
+def calcMean(val):
+    return (sum(val)/len(val))
 
-V = []
 
-for l in f:
-    A = [int(x) for x in l.split()]
-    V.append(A[col_num])
+# Function to calculate the stdard deviation of a integer vector
+def calcStdev(val, mean_in):
+    return math.sqrt(sum([(mean_in-x)**2 for x in val]) / len(val))
 
-mean = sum(V)/len(V)
 
-stdev = math.sqrt(sum([(mean-x)**2 for x in V]) / len(V))
+# Parsing arguments using argparse
+def parseArgs():
+    parser = ap.ArgumentParser(description="Process filename and columns",
+                               prog='get_column_stats')
 
-print('mean:', mean)
-print('stdev:', stdev)
+    parser.add_argument('-i',
+                        '--input',
+                        type=str,
+                        help="Input file name",
+                        required=True)
+
+    parser.add_argument('-c',
+                        '--column',
+                        type=int,
+                        help="Number of columns in the input file",
+                        required=True)
+
+    return parser.parse_args()
+
+
+# Main
+def main():
+    args = parseArgs()
+    filename = args.input
+    # Fix the 0 based indexing in python, so the colu
+    ncol = args.column - 1
+
+    f = None
+    mean = None
+    stdev = None
+    V = []
+
+    # Check if the input file is valid
+    try:
+        inputFile = open(args.input, 'r')
+    except FileNotFoundError:
+        print("The specified input file does not exist. Exiting.")
+        sys.exit(1)
+
+    # Check if the input column number is valid
+    if ncol < 0:
+        raise ValueError('Column number must be greater than 0.')
+        sys.exit(1)
+
+    # Append a new vector with the numbers in the specified column
+    for row in inputFile:
+        try:
+            A = [int(x) for x in row.split()]
+            V.append(A[ncol])
+        except ValueError:
+            print('Input file contains bad formatting please check.')
+            sys.exit(1)
+
+    # Check if the previous operation is completed successfully
+    if len(V) == 0:
+        raise Exception('None of the values are valid for calculation')
+    else:
+        mean = calcMean(V)
+        stdev = calcStdev(V, mean)
+
+    # Return the results
+    print('mean:', mean)
+    print('stdev:', stdev)
+
+
+if __name__ == "__main__":
+    main()
